03c0113efe0757a4e9afe203937c80336bf2dcbe
Danila <d.v.valko@gmail.com>
2024-09-26 17:27:03 +0200

    reorganized folders

---
+++ examples/scalability/cells.py
@@ -0,0 +1,174 @@
+import os
+import json
+import random
+import pandas as pd
+import numpy as np
+import pandapower as pp
+import pandapower.networks as pn
+from pandapower.toolbox import merge_nets
+
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+#import pandapower.auxiliary
+#pandapower.auxiliary._check_if_numba_is_installed = lambda x: x
+
+'''
+Cell based grid generating script
+
+'''
+
+def set_random_seed(seed=13):
+    random.seed(seed)
+    np.random.seed(seed)
+    return seed
+
+def get_random_seed(base_seed=None, fixed_range=1000):
+    if base_seed:
+        set_random_seed(base_seed)
+    return random.randint(0, fixed_range)
+
+def get_cell():
+    '''
+    https://pandapower.readthedocs.io/en/v2.10.0/networks/cigre.html#medium-voltage-distribution-network-with-pv-and-wind-der
+    This pandapower network includes the following parameter tables:
+    - switch (8 elements)
+    - load (18 elements)
+    - ext_grid (1 elements)
+    - sgen (9 elements)
+    - line (15 elements)
+    - trafo (2 elements)
+    - bus (15 elements)
+    - bus_geodata (15 elements)
+    '''
+    def _switch(net, type='CB', et='t', element=1, bus=0, closed=False): 
+        net.switch.loc[(net.switch['type'] == type) &
+                       (net.switch['et'] == et) &
+                       (net.switch['element'] == element) &
+                       (net.switch['bus'] == bus), 'closed']  = closed
+    
+    net = pn.create_cigre_network_mv(with_der="pv_wind") # gets a cell subnetwork
+    _switch(net, closed=False) # switching off the right part (trafo 1, see the scheme) of the subnetwork
+    return net
+
+def create_cells(cells_count=2, dir='./', validation=False):
+    '''
+    Creates a network that consists of ´cells_count´ connected CIGRE subnetworks.
+    Saves the network json file `cells.json` to `dir`.
+    '''
+    def _rename(elements, cutoff_index, ren_type=None): # create new name for each unit 'type-index-bus-cell'
+        for idx, g in elements.iterrows():
+            new_type = g.type.lower() if not ren_type else ren_type
+            elements.loc[elements.index == idx, 'type'] = new_type
+            elements.loc[elements.index == idx, 'name'] = f"{new_type}-{idx}-{g.bus}-{int(g.bus / cutoff_index) if cutoff_index > 0 else 0}"
+
+    def _save_old_names(elements, mapping, ren_type=None):
+        for idx, g in elements.iterrows():
+            new_type = g.type.lower() if not ren_type else ren_type
+            if idx in mapping:
+                elements.loc[elements.index == idx, 'type'] = new_type
+                elements.loc[elements.index == idx, 'name'] = f"{new_type}-{idx}-{idx}" 
+            else:  
+                for old_idx, new_idx in mapping.items():
+                    if idx == new_idx:
+                        elements.loc[elements.index == idx, 'type'] = new_type
+                        elements.loc[elements.index == idx, 'name'] = f"{new_type}-{idx}-{old_idx}"
+                        break
+
+    cell_cutoff = 0
+    net = get_cell()
+    for _ in range(cells_count-1):
+        new_subnet = get_cell()
+        cell_cutoff = len(new_subnet.bus) # identify the cell index based on the index of buses
+        net, idx = merge_nets(net, new_subnet, validation=False, numba=False, return_net2_reindex_lookup=True)
+        _save_old_names(net.bus, idx['bus'], ren_type='Bus')
+        pp.create_line(net, from_bus=0, to_bus=idx['bus'][0], length_km=5, std_type="NAYY 4x50 SE") # connect merged cells
+        net.ext_grid.drop(1, inplace=True) # drop excessive external grid
+        net.res_ext_grid.drop(1, inplace=True)
+    _rename(net.sgen, cell_cutoff, ren_type='StaticGen')
+    _rename(net.load, cell_cutoff, ren_type='Load')
+    _rename(net.ext_grid, cell_cutoff, ren_type='ExternalGrid')
+
+    if validation:
+        pp.runpp(net, numba=False) # to test
+    if dir:
+        dir = os.path.join(dir, "cells.json")
+        pp.to_json(net, dir)
+    return net, dir
+
+def generate_profiles(net, dir='./', seed=13):
+    profiles = {}
+    random.seed(seed)
+    np.random.seed(seed)
+    for _, unit in net.sgen.iterrows():
+        p = unit.p_mw * random.randrange(1, 4)
+        profiles[unit['name']] = {
+                'max' : p,
+                'min' : 0,
+            }
+    for _, unit in net.load.iterrows():
+        p = unit.p_mw * random.randrange(1, 4)
+        profiles[unit['name']] = {
+                'max' : p,
+                'min' : (p - p * random.randrange(1, 100) / 100) * random.randrange(0, 1) + random.randrange(0, 5) / 10,
+            }
+    for _, unit in net.res_ext_grid.iterrows():
+        p = unit.p_mw
+        profiles[net.ext_grid.iloc[unit.name]['name']] = {
+                'max' : p * 50,
+                'min' : 0,
+            }
+    if dir:
+        dir = os.path.join(dir, "profiles.json")
+        with open(dir, 'w') as f:
+            json.dump(profiles, f)
+    return profiles, dir
+
+
+def get_cells_data(grid, grid_extra_info, profiles):
+    def lookup_bus_name(idx):
+        return grid_extra_info[f'Bus-{idx}']['name']
+
+    cells = {}
+    for e in grid.children:
+        if e.eid in grid_extra_info and\
+           'name' in grid_extra_info[e.eid] and\
+           pd.notna(grid_extra_info[e.eid]['name']):
+                name = grid_extra_info[e.eid]['name']
+                id = name.split('-')
+                if len(id) == 4: # type-index-bus-cell
+                    cells.setdefault(id[3], {})
+                    cells.setdefault('match_cell', {})
+                    cells['match_cell'].update({e.eid : id[3]})
+                    cells[id[3]].setdefault(id[0], {})
+                    cells[id[3]][id[0]].update({e.eid : {
+                        'unit' : e,
+                        'type' : id[0],
+                        'index' : id[1],
+                        'bus' : lookup_bus_name(id[2]),
+                        'cell' : id[3],
+                        'profile' : profiles[name] if name in profiles else {},
+                    }})
+    return cells
+
+def get_unit_profile(aeid, cells_data):
+    if aeid == 'MosaikAgent':
+        unit_eid = cells_data['match_agent'][aeid]
+        unit_type = unit_eid.split('-')[0]
+        return cells_data[cells_data['match_cell'][unit_eid]][unit_type][unit_eid]['profile']
+    if aeid in cells_data['match_agent']:
+        sim_eid = cells_data['match_agent'][aeid]
+        if sim_eid in cells_data['match_unit']:
+            unit_eid = cells_data['match_unit'][sim_eid]
+            unit_type = unit_eid.split('-')[0]
+            if unit_eid in cells_data['match_cell']:
+                return cells_data[cells_data['match_cell'][unit_eid]][unit_type][unit_eid]['profile']
+    return {}
+
+if __name__ == '__main__':
+    cells_count = 2
+    net, net_file = create_cells(cells_count=cells_count)
+    print(f'The network of {cells_count} cells is created and saved at: {net_file}')
+    print(net)
+    profiles, prof_file = generate_profiles(net)
+    print(f'The profiles were generated and saved at: {prof_file}')
+    print(profiles)
---
+++ examples/scalability/methods.py
@@ -0,0 +1,343 @@
+import copy
+import numpy as np
+from mosaik_components.mas.utils import *
+
+MAS_STATE = MAS_DEFAULT_STATE.copy()
+
+def state_to_output(aeid, aid, attrs, current_state, converged, current_time_step, first_time_step, **kwargs):
+# inputs {'Agent_3': {'current': {'WecsSim-0.wecs-0': 147.26366926766127}},
+# output_state: {'Agent_1': {'production': {'min': 0, 'max': 0, 'current': 1.0}, 'consumption': {'min': 0, 'max': 0, 'current': 0.0}},
+# entities: {'Agent_3': 'WecsSim-0.wecs-0', 'Agent_4': 'Grid-0.Load-0',
+# Agent_6 ['scale_factor'] {'production': {'min': 0, 'max': 0.02, 'current': 0.02, 'scale_factor': 1.0}, 'consumption': {'min': 0, 'max': 0, 'current': 0, 'scale_factor': 1}}
+    data = {}
+
+    if abs(current_state['production']['current']) > PRECISION or abs(current_state['production']['scale_factor']) > PRECISION:
+        key = 'production'
+    elif abs(current_state['consumption']['current']) > PRECISION or abs(current_state['consumption']['scale_factor']) > PRECISION:
+        key = 'consumption'
+    else:
+        key = 'production'
+
+    current = current_state[key]['current']
+    scale_factor = current_state[key]['scale_factor']
+
+    for attr in attrs:
+        if 'current' == attr:
+            data.update({'current' : current})
+        elif 'scale_factor' == attr:
+            if not converged:
+                data.update({'scale_factor' : scale_factor})
+
+    return data
+
+
+def aggregate_states(aeid, aid, requested_states, current_state=MAS_STATE, **kwargs):
+    current_state = copy.deepcopy(current_state)
+    for aid, state in requested_states.items():
+        for i in current_state.keys():
+            for j in current_state[i].keys():
+                    if isinstance(current_state[i][j], (int, float, list)):
+                        current_state[i][j] += state[i][j]
+    return current_state
+
+def compute_instructions(current_state, **kwargs):
+
+    verbose = kwargs.get('verbose', 0)
+    def calc_delta(current_state, new_state):
+            _new_state = copy.deepcopy(new_state)
+            for i in new_state.keys():
+                for j in new_state[i].keys():
+                    if isinstance(_new_state[i][j], (int, float)):
+                        _new_state[i][j] -= current_state[i][j]
+            return _new_state
+
+    def compose_instructions(agents_info, delta):
+        _delta = copy.deepcopy(delta)
+        _agents_info = copy.deepcopy(agents_info)
+        for aid, state in _agents_info.items():
+            for i in _delta.keys():
+                #if i != 'info':
+                    max_inc = state[i]['max'] - state[i]['current']
+                    max_dec = state[i]['current'] - state[i]['min']
+                    if _delta[i]['current'] > PRECISION:
+                        if _delta[i]['current'] <= max_inc:
+                            state[i]['current'] += _delta[i]['current']
+                            _delta[i]['current'] = 0
+                        else:
+                            state[i]['current'] += max_inc
+                            _delta[i]['current'] -= max_inc
+                    elif _delta[i]['current'] <= PRECISION:            
+                        if abs(_delta[i]['current']) <= max_dec:
+                            state[i]['current'] += _delta[i]['current']
+                            _delta[i]['current'] = 0
+                        else:
+                            state[i]['current'] -= max_dec
+                            _delta[i]['current'] += max_dec
+                    
+                    state[i]['scale_factor'] = state[i]['current'] - agents_info[aid][i]['current']#/ if agents_info[aid][i]['current'] > PRECISION else 1
+
+        return _agents_info, _delta
+    
+    def compute_balance(external_network_state,#=copy.deepcopy(MAS_STATE), 
+                                    cells_aggregated_state):#=copy.deepcopy(MAS_STATE)):
+                
+                #cells_aggregated_state = copy.deepcopy(cells_aggregated_state)
+                #external_network_state = copy.deepcopy(external_network_state)               
+                old_cells_aggregated_state = copy.deepcopy(cells_aggregated_state)
+                old_external_network_state = copy.deepcopy(external_network_state)
+                cell_balance = cells_aggregated_state['production']['current'] - cells_aggregated_state['consumption']['current']
+                cells_aggregated_state['production']['current'] = np.clip(cells_aggregated_state['production']['current'], 
+                                                                            cells_aggregated_state['production']['min'], 
+                                                                            cells_aggregated_state['production']['max'])
+                cells_aggregated_state['consumption']['current'] = np.clip(cells_aggregated_state['consumption']['current'], 
+                                                                            cells_aggregated_state['consumption']['min'], 
+                                                                            cells_aggregated_state['consumption']['max'])
+                cell_expected_balance = cells_aggregated_state['production']['current'] - cells_aggregated_state['consumption']['current']
+
+                external_network_min = external_network_state['consumption']['max'] * (-1)
+                external_network_max = external_network_state['production']['max']
+                external_network_default_balance = np.clip(external_network_state['production']['current'] - external_network_state['consumption']['current'],# - cell_balance,
+                                                            external_network_min,
+                                                            external_network_max)
+                if verbose >= 0:
+                    print(highlight('cells balance:', 'red'), cell_balance)
+                    print(highlight('cells expected balance:', 'red'), cell_expected_balance)
+                    print(highlight('external network default balance:', 'blue'), external_network_default_balance)
+                #print('flexibility:')
+                #print('external_network_min', external_network_min)
+                #print('external_network_max', external_network_max)
+
+                cell_inc_production = cells_aggregated_state['production']['max'] - cells_aggregated_state['production']['current']
+                if cell_inc_production < PRECISION:
+                    cell_inc_production = 0
+                cell_dec_production = cells_aggregated_state['production']['current'] - cells_aggregated_state['production']['min']
+                if cell_dec_production < PRECISION:
+                    cell_dec_production = 0
+                cell_dec_consumption = cells_aggregated_state['consumption']['current'] - cells_aggregated_state['consumption']['min']
+                if cell_dec_consumption < PRECISION:
+                    cell_dec_consumption = 0
+                cell_inc_consumption = cells_aggregated_state['consumption']['max'] - cells_aggregated_state['consumption']['current']
+                if cell_inc_consumption < PRECISION:
+                    cell_inc_consumption = 0
+                
+                #print('cell_inc_production', cell_inc_production)
+                #print('cell_dec_production', cell_dec_production)
+                #print('cell_inc_consumption', cell_inc_consumption)
+                #print('cell_dec_consumption', cell_dec_consumption)
+                #print()
+
+                grid_inc = external_network_max - external_network_default_balance
+                grid_dec = external_network_default_balance - external_network_min
+                #print('grid_inc', grid_inc)
+                #print('grid_dec', grid_dec)
+
+                if cell_expected_balance < PRECISION:
+                    if abs(cell_expected_balance) <= cell_inc_production:
+                        cell_inc_production = abs(cell_expected_balance)
+                        #cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_inc_production + cell_dec_consumption:
+                        cell_dec_consumption = abs(cell_expected_balance) - cell_inc_production
+                        #cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        #cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+                        
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_inc_production + cell_dec_consumption + grid_inc:
+                        grid_inc = abs(cell_expected_balance) - cell_inc_production - cell_dec_consumption
+                        #cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        #cell_dec_consumption = 0
+                        #grid_inc = 0
+                        grid_dec = 0
+                        
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_inc_production + cell_dec_consumption + grid_inc + grid_dec:
+                        grid_dec = abs(cell_expected_balance) - cell_inc_production - cell_dec_consumption - grid_inc
+                        #cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        #cell_dec_consumption = 0
+                        #grid_inc = 0
+                        #grid_dec = 0
+                        
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    else:
+                        if verbose >= 0:
+                            print('balance mismatch!')
+                        cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0 
+                elif cell_expected_balance > PRECISION:
+                    if abs(cell_expected_balance) <= cell_dec_production:
+                        cell_dec_production = abs(cell_expected_balance)
+                        cell_inc_production = 0
+                        #cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_dec_production + cell_inc_consumption:
+                        cell_inc_consumption = abs(cell_expected_balance) - cell_dec_production
+                        cell_inc_production = 0
+                        #cell_dec_production = 0
+                        #cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_dec_production + cell_inc_consumption + grid_dec:
+                        grid_dec = abs(cell_expected_balance) - cell_dec_production - cell_inc_consumption
+                        cell_inc_production = 0
+                        #cell_dec_production = 0
+                        #cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        #grid_dec = 0
+
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0
+                    elif abs(cell_expected_balance) <= cell_dec_production + cell_inc_consumption + grid_dec + grid_inc:
+                        grid_inc = abs(cell_expected_balance) - cell_dec_production - cell_inc_consumption - grid_dec
+                        cell_inc_production = 0
+                        #cell_dec_production = 0
+                        #cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        #grid_inc = 0
+                        #grid_dec = 0
+                    else:
+                        if verbose >= 0:
+                            print('balance mismatch!')
+                        cell_inc_production = 0
+                        cell_dec_production = 0
+                        cell_inc_consumption = 0
+                        cell_dec_consumption = 0
+                        grid_inc = 0
+                        grid_dec = 0
+                        #grid_inc_production = 0
+                        #grid_dec_production = 0
+                        #grid_inc_consumption = 0
+                        #grid_dec_consumption = 0 
+                else:
+                    cell_inc_production = 0
+                    cell_dec_production = 0
+                    cell_inc_consumption = 0
+                    cell_dec_consumption = 0
+                    grid_inc = 0
+                    grid_dec = 0
+                    if verbose >= 0:
+                        print('perfect balance!')  
+                #print()           
+                #print('control action:')
+                #print('cell_inc_production', cell_inc_production) 
+                #print('cell_dec_production', cell_dec_production) 
+                #print('cell_dec_consumption', cell_dec_consumption) 
+                #print('cell_inc_consumption', cell_inc_consumption) 
+                #print('grid_inc', grid_inc) 
+                #print('grid_dec', grid_dec) 
+
+                #print('grid_inc_production', grid_inc_production)
+                #print('grid_dec_production', grid_dec_production)
+                #print('grid_dec_consumption', grid_dec_consumption) 
+                #print('grid_inc_consumption', grid_inc_consumption)
+
+                cells_aggregated_state['production']['current'] = cells_aggregated_state['production']['current'] + cell_inc_production - cell_dec_production
+                cells_aggregated_state['consumption']['current'] = cells_aggregated_state['consumption']['current'] + cell_inc_consumption - cell_dec_consumption     
+                new_cell_balance = cells_aggregated_state['production']['current'] - cells_aggregated_state['consumption']['current']
+                
+                new_external_network_balance = external_network_default_balance + grid_inc - grid_dec
+                if new_external_network_balance > PRECISION:
+                    external_network_state['production']['current'] = new_external_network_balance
+                    external_network_state['consumption']['current'] = 0
+                else:
+                    external_network_state['production']['current'] = 0
+                    external_network_state['consumption']['current'] = abs(new_external_network_balance)      
+
+                cells_aggregated_delta = calc_delta(old_cells_aggregated_state, cells_aggregated_state)
+                external_network_delta = calc_delta(old_external_network_state, external_network_state)
+
+                #print('new_cells_aggregated_state', cells_aggregated_state)
+                #print('new_external_network_state', external_network_state)
+                #print('cells_aggregated_delta', cells_aggregated_delta)
+                #print('external_network_delta', external_network_delta)
+
+                if verbose >= 0:
+                    print(highlight('new cells balance:', 'red'), new_cell_balance)
+                    print(highlight('external network balance:', 'blue'), new_external_network_balance)
+
+                ok = abs(new_cell_balance - cell_expected_balance) < PRECISION
+                #ok = abs(cell_balance - cell_expected_balance) < PRECISION
+                #ok = abs(cell_balance - new_cell_balance) < PRECISION
+                if verbose >= 0:
+                    if ok:
+                       print(highlight('balanced solution', 'blue'))
+                return ok, cells_aggregated_state, cells_aggregated_delta, external_network_state, external_network_delta
+
+    requested_states = kwargs.get('requested_states', {})
+    instruction = kwargs.get('instruction', None)
+
+    if instruction != None:
+        ok = True
+        state = None
+        delta = calc_delta(current_state, instruction)
+        instructions, remains = compose_instructions(requested_states, delta)
+    else:
+        cells_aggregated_state = aggregate_states(None, None, requested_states)
+        ok, _, cells_delta, state, _ = compute_balance(current_state, cells_aggregated_state)
+        instructions, remains = compose_instructions(requested_states, cells_delta)
+
+    return ok, instructions, state
+
+def execute_instructions(aeid, aid, instruction, current_state, requested_states, **kwargs):
+    ok = True   
+
+    if not len(requested_states) == 0:
+        ok, instructions, state = compute_instructions(instruction=instruction, 
+                                                                    current_state=current_state,
+                                                            requested_states=requested_states, **kwargs)
+        if aeid != 'MosaikAgent':
+            state = MAS_STATE.copy()
+    else:
+        instructions = {aid : instruction}
+        state = instruction
+
+    return ok, instructions, state
