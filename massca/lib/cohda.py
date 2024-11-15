a4f07c62e02f971a2d2f8cfee378530e6f7ca763
Danila Valko <d.v.valko@gmail.com>
2024-11-15 18:50:19 +0100

    clean cohda

--- massca/lib/cohda.py
+++ massca/lib/cohda.py
@@ -1,77 +1,16 @@
 import sys, os, copy
-old_stdout = sys.stdout
-sys.stdout = open(os.devnull, "w")
+import numpy as np
+import nest_asyncio
 import logging
 import asyncio
 import random
-import numpy as np
-import nest_asyncio
-import mosaik_api_v3 as mosaik_api
-from mango import Agent, RoleAgent
-from mosaik_cohda.des_flex.flex_class import Flexibility
-from mosaik_cohda.start_values import StartValues
-from mosaik_cohda.cohda_simulator import CohdaSimulator
-from mosaik_cohda.agent_roles import FlexReceiverRole, FlexCohdaRole, \
-    FlexTerminationRole, FlexNegotiationStarterRole
-from mosaik_cohda.start_values import StartValues, SolutionSchedule
-from mosaik_cohda.mango_library.coalition.core import CoalitionParticipantRole
-from mosaik_cohda.cohda_simulator import logger
 
 from ..utils import make_hashable
-
+old_stdout = sys.stdout
+sys.stdout = open(os.devnull, "w")
+from mosaik_cohda.cohda_simulator import Simulator, Flexibility, StartValues
 sys.stdout = old_stdout
 
-class FlexReceiverRoleModified(FlexReceiverRole):
-    def setup(self) -> None:
-        super().setup()
-        self.id = int(self.context.aid.split('agent')[1]) + 1
-
-    def on_change_model(self, model) -> None:
-        if model.terminated:
-            schedule = self.context.data.current_cohda_solution.solution_candidate.candidate[self.id]
-            solution = SolutionSchedule(schedule)
-            self.context.schedule_instant_task(
-                self.context.send_acl_message(
-                    receiver_addr=self._mosaik_agent_address[0],
-                    receiver_id=self._mosaik_agent_address[1],
-                    content=solution,
-                    acl_metadata={'conversation_id':
-                                      self._mosaik_agent_address[1],
-                                  'sender_id': self.context.aid},
-                )
-            )
-
-class Simulator(CohdaSimulator):
-
-    async def create_flex_agent(self, container,
-                                time_resolution, initiator: bool = False) -> Agent:
-        a = RoleAgent(container)
-        a.add_role(CoalitionParticipantRole())
-        a.add_role(FlexCohdaRole(time_resolution=time_resolution))
-        a.add_role(FlexReceiverRoleModified((self.main_container.addr,
-                                     self.mosaik_agent.aid)))
-        a.add_role(FlexTerminationRole(initiator))
-        if initiator:
-            a.add_role(FlexNegotiationStarterRole())
-        return a
-
-    @staticmethod
-    async def _shutdown(*args):
-        futs = []
-        for arg in args:
-            futs.append(arg.shutdown())
-        await asyncio.gather(*futs)
-
-    def finalize(self):
-        try:
-            if not self.loop.is_closed():
-                self.loop.run_until_complete(
-                    self._shutdown(self.mosaik_agent,
-                                self.agent_container,
-                                self.main_container
-                                ))
-        except:
-            pass
         
 class COHDA():
     def __init__(self, step_size=15*60, time_resolution=1., 
